7617dc67db6f974239fae37aad907b661a1c5968
putuphillipsteven <putu.phillip@gmail.com>
2024-10-30 21:15:23 +0700

    just testing

--- src/main/java/com/pps/test/TestTaskRepository.java
+++ src/main/java/com/pps/test/TestTaskRepository.java
@@ -12,7 +12,7 @@ public class TestTaskRepository {
 
     public static void main(String[] args) {
         testViewTask();
-        testMarkToDo();
+        testAddTask();
         testViewTask();
     }
 
